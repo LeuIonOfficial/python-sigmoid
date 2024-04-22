877a4f9128b742d797d3e213933814bdde1555a5
Ion Leu <leuionofficial@gmail.com>
2024-04-22 17:14:04 +0300

    Add partial homework for lesson 8

---
+++ lesson_8/task_1.py
@@ -0,0 +1,219 @@
+# Aceasta este tema pentru lecția 8 legată de loops
+
+# Creați o listă de numere de la 1 la 10 folosind un for loop și funcția range().
+
+# CODUL TĂU VINE MAI JOS:
+my_list = [i for i in range(1, 11)]
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind un for loop, parcurgeți lista creată și afișați numai numerele pare.
+
+# CODUL TĂU VINE MAI JOS:
+print([i for i in my_list if i % 2 == 0])
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind un while loop, creați o variabilă 'i' inițializată cu 1 și incrementați-o până când ajunge la 5. Afișați valoarea 'i' la fiecare pas.
+
+# CODUL TĂU VINE MAI JOS:
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+# CODUL TĂU VINE MAI SUS:
+
+# Creați un dicționar person cu cheile 'name', 'age' și 'city' și iterați prin dicționar afișând perechile de cheie-valoare.
+
+# CODUL TĂU VINE MAI JOS:
+person = {
+    'name': "Ion",
+    'age': 22,
+    'city': "Chisinau"
+}
+
+for attr, value in person.items():
+    print(f"{attr} - {value}")
+# CODUL TĂU VINE MAI SUS:
+
+# Utilizați un for loop pentru a itera printr-o listă de liste (matrice) și afișați fiecare element.
+
+# CODUL TĂU VINE MAI JOS:
+data = [
+    [
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]
+    ],
+    [
+        [2, 3, 4],
+        [2, 3, 4],
+        [2, 3, 4]
+    ],
+    [
+        [3, 4, 5],
+        [3, 4, 5],
+        [3, 4, 5]
+    ],
+    [
+        [4, 5, 6],
+        [4, 5, 6],
+        [4, 5, 6]
+    ],
+]
+
+for arr in data:
+    for numbers in arr:
+        for number in numbers:
+            print(number)
+# CODUL TĂU VINE MAI SUS:
+
+# Creați o secvență de numere folosind funcția range() și apoi iterați prin această secvență folosind un for loop pentru a afișa numerele.
+
+# CODUL TĂU VINE MAI JOS:
+numere = [num for num in range(50)]
+for num in numere:
+    print(num)
+# CODUL TĂU VINE MAI SUS:
+
+# Folosiți funcția enumerate() pentru a itera printr-o listă și a afișa indexul fiecărui element alături de valoarea sa.
+
+# CODUL TĂU VINE MAI JOS:
+enum_numbers = enumerate(numere)
+for attr, value in enum_numbers:
+    print(attr)
+# CODUL TĂU VINE MAI SUS:
+
+# Folosiți funcția zip() pentru a itera simultan prin două liste și a afișa elementele corespunzătoare.
+
+# CODUL TĂU VINE MAI JOS:
+zipped_list = zip(numere, my_list)
+print(list(zipped_list))
+# CODUL TĂU VINE MAI SUS:
+
+# Creați o listă de numere de la 1 la 10 folosind un for loop și funcția range().
+
+# CODUL TĂU VINE MAI JOS:
+list_of_numbers = [num for num in range(1,11)]
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind un buclă while, dublează valorile listei până când primul element va deveni mai mare decât 50.
+
+# CODUL TĂU VINE MAI JOS:
+while list_of_numbers[0] <= 50:
+    for index, element in enumerate(list_of_numbers):
+        list_of_numbers[index] = element * 2
+# CODUL TĂU VINE MAI SUS:
+
+# Generează și printează o listă cu toate numerele pătrat perfect din intervalul [1, 100].
+
+# CODUL TĂU VINE MAI JOS:
+print([num for num in range(1, 100) if (num ** 0.5).is_integer()])
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind un buclă for , printează tabla înmulțirii pentru numărul 7.
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Creează o listă de liste, unde fiecare sub-listă conține perechi (i, j) pentru i și j de la 1 la 5. Printează această listă de perechi.
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Parcurge lista de la punctul anterior și printează doar perechile unde i < j .
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind un buclă while , caută și printează prima valoare care este mai mare decât 10 dintr-o listă cu numere random creată de tine. Dacă nu există, printează "Nu există valori mai mari decât 10".
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind loop-uri Creează un pătrat de stele ( * ) folosind bucle încadrate. Dimensiunea pătratului va fi citită de la utilizator.
+# Exemplu pentru un pătrat de dimensiune 5:
+# *****
+# *****
+# *****
+# *****
+# *****
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind for sau while loops realizați afișările de mai jos
+
+# Afișarea 1:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+# 123456
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Afișarea 2:
+
+# 54321
+# 5432
+# 543
+# 54
+# 5
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Afișarea 3:
+
+# abcdefg
+# bcdefg
+# cdefg
+# defg
+# efg
+# fg
+# g
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Afișarea 4:
+
+# +-+-+-+-+-+-+-+-
+# -+-+-+-+-+-+-+-+
+# +-+-+-+-+-+-+-+-
+# -+-+-+-+-+-+-+-+
+# +-+-+-+-+-+-+-+-
+# -+-+-+-+-+-+-+-+
+# +-+-+-+-+-+-+-+-
+# -+-+-+-+-+-+-+-+
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Afișarea 5:
+
+# 3
+# 3 9
+# 3 9 27
+# 3 9 27 81
+# 3 9 27 81 243
+# 9 27 81 243
+# 27 81 243
+# 81 243
+# 243
+
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Completați sarcinile de mai sus pentru a exersa lucrul cu buclele în Python.
